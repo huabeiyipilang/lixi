fd6487f3bc09098e3c2c19d48e802a78ec899497
Carl Li <huabeiyipilang@gmail.com>
2018-04-28 20:33:04 +0800

    引入公历农历转换js脚本

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+declare var calendar: any;
 
 const BIRTHDAY = 1478348880000;
 const A_DAY = 24 * 60 * 60 * 1000;
+const NONGLI_BIRTHDAYS = ["2016-11-05"];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 
 export class AppComponent implements OnInit {
-  
+
   birthdayDate: Date;
+  calendarInfos: any;
 
   ngOnInit() {
     this.birthdayDate = new Date();
     this.birthdayDate.setTime(BIRTHDAY);
-    console.log('ngOnInit:' + this.birthdayDate);
+    this.calendarInfos = calendar.solar2lunar(this.birthdayDate.getFullYear(), this.birthdayDate.getMonth(), this.birthdayDate.getDay());
+    console.log('ngOnInit:' + this.calendarInfos);
   }
 
   getFormatBirthday() {
@@ -23,6 +28,10 @@ export class AppComponent implements OnInit {
   }
 
   getDaysAfterBirthday() {
-    return Math.floor((new Date().getTime() - BIRTHDAY + A_DAY) / A_DAY); 
+    return Math.floor((new Date().getTime() - BIRTHDAY + A_DAY) / A_DAY);
+  }
+
+  getDaysBeforeNextBirthday() {
+    return this.calendarInfos.Animal;
   }
 }

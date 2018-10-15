e24ce1451f61e2781097a336c510c58e09d40869
Carl Li <huabeiyipilang@gmail.com>
2018-10-15 17:35:51 +0800

    使用服务器数据

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 declare var calendar: any;
 
 const BIRTHDAY = 1478348880000;
@@ -16,26 +17,24 @@ export class AppComponent implements OnInit {
   birthdayDate: Date;
   birthdayDateStart: Date;
   //农历生日对象
-  calendarInfos: any;
-
+  lunarDate: any;
+  respObject: any;
+  constructor(private http: HttpClient) {
+  }
   ngOnInit() {
-    this.birthdayDate = new Date();
-    this.birthdayDate.setTime(BIRTHDAY);
-
-    this.birthdayDateStart = new Date();
-    this.birthdayDateStart.setTime(BIRTHDAY);
-    this.birthdayDateStart.setHours(0);
-    this.birthdayDateStart.setMinutes(0);
-    this.birthdayDateStart.setMilliseconds(0);
-
-    this.calendarInfos = calendar.solar2lunar(this.birthdayDate.getFullYear(), this.birthdayDate.getMonth(), this.birthdayDate.getDay());
-    console.log('ngOnInit:' + this.calendarInfos);
+    this.http.get('http://176.122.189.169:28789/lixi/bithdayinfo').subscribe(data => {
+      this.respObject = data;
+      this.birthdayDate = new Date(this.respObject.birthTime);
+    });
   }
 
   /**
    * 格式生日
    */
   getFormatBirthday() {
+    if (!this.birthdayDate) {
+      return "";
+    }
     return this.birthdayDate.getFullYear() + "年" + this.birthdayDate.getMonth() + "月" + this.birthdayDate.getDay() + "日"
       + " " + this.birthdayDate.getHours() + ":" + this.birthdayDate.getMinutes();
   }
@@ -44,15 +43,49 @@ export class AppComponent implements OnInit {
    * 出生天数
    */
   getDaysAfterBirthday() {
-    return Math.floor((new Date().getTime() - this.birthdayDateStart.getTime() + A_DAY) / A_DAY);
+    if (!this.respObject) {
+      return "";
+    }
+    return this.respObject.liveDays;
+  }
+
+  /**
+   * 下一个农历生日的阳历日期
+   */
+  getSolarDateNextLunarBirthday() {
+    if (!this.respObject) {
+      return "";
+    }
+    return this.respObject.nextBirthday;
   }
 
   /**
    * 距下一个生日还有多少天
    */
   getDaysBeforeNextBirthday() {
-    let currentDate = new Date();
-    let currentLunar = calendar.solar2lunar(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDay());
-    return this.calendarInfos.Animal;
+    if (!this.respObject) {
+      return "";
+    }
+    return this.respObject.nextBirthdayCountdown;
+  }
+
+  /**
+   * 农历出生日期
+   */
+  getLunarFormatDate() {
+    if (!this.respObject) {
+      return "";
+    }
+    return this.respObject.lunarDate;
+  }
+
+  /**
+   * 属相
+   */
+  getAnimal() {
+    if (!this.respObject) {
+      return "";
+    }
+    return this.respObject.animal;
   }
 }

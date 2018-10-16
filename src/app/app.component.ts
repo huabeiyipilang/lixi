9c00eef1e1db06835a6eb66923cb8d47cc4e39e7
Carl Li <huabeiyipilang@gmail.com>
2018-10-16 21:52:45 +0800

    与服务器对接

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { HttpParams } from "@angular/common/http";
+
 declare var calendar: any;
 
 const BIRTHDAY = 1478348880000;
 const A_DAY = 24 * 60 * 60 * 1000;
+const DEBUG = false;
 
 @Component({
   selector: 'app-root',
@@ -12,80 +16,24 @@ const A_DAY = 24 * 60 * 60 * 1000;
 })
 
 export class AppComponent implements OnInit {
-
-  //生日Date对象
-  birthdayDate: Date;
-  birthdayDateStart: Date;
-  //农历生日对象
-  lunarDate: any;
   respObject: any;
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: ActivatedRoute) {
   }
   ngOnInit() {
-    this.http.get('http://176.122.189.169:28789/lixi/bithdayinfo').subscribe(data => {
-      this.respObject = data;
-      this.birthdayDate = new Date(this.respObject.birthTime);
-    });
-  }
-
-  /**
-   * 格式生日
-   */
-  getFormatBirthday() {
-    if (!this.birthdayDate) {
-      return "";
-    }
-    return this.birthdayDate.getFullYear() + "年" + this.birthdayDate.getMonth() + "月" + this.birthdayDate.getDay() + "日"
-      + " " + this.birthdayDate.getHours() + ":" + this.birthdayDate.getMinutes();
-  }
-
-  /**
-   * 出生天数
-   */
-  getDaysAfterBirthday() {
-    if (!this.respObject) {
-      return "";
-    }
-    return this.respObject.liveDays;
-  }
-
-  /**
-   * 下一个农历生日的阳历日期
-   */
-  getSolarDateNextLunarBirthday() {
-    if (!this.respObject) {
-      return "";
-    }
-    return this.respObject.nextBirthday;
-  }
-
-  /**
-   * 距下一个生日还有多少天
-   */
-  getDaysBeforeNextBirthday() {
-    if (!this.respObject) {
-      return "";
-    }
-    return this.respObject.nextBirthdayCountdown;
-  }
-
-  /**
-   * 农历出生日期
-   */
-  getLunarFormatDate() {
-    if (!this.respObject) {
-      return "";
-    }
-    return this.respObject.lunarDate;
-  }
-
-  /**
-   * 属相
-   */
-  getAnimal() {
-    if (!this.respObject) {
-      return "";
-    }
-    return this.respObject.animal;
+    this.router
+      .queryParams
+      .subscribe(queryParams => {
+        if (!queryParams['name'] || !queryParams['date']) {
+          return;
+        }
+        let urlParam = new HttpParams().set('name', queryParams['name']).set('date', queryParams['date']);
+        let url: string = DEBUG ? 'http://127.0.0.1:8080/bithdayinfo' : 'http://176.122.189.169:28789/lixi/bithdayinfo';
+        this.http.get(url, { params: urlParam })
+          .subscribe(data => {
+            this.respObject = data;
+            console.log(this.respObject.name);
+            console.log(this.respObject);
+          });
+      });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+  { path: '', component: AppComponent }
+];
 
 @NgModule({
   declarations: [
@@ -12,6 +16,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
